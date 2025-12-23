210321342948e4326b094ac67a0310758dbdb8a7
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-12-23 21:53:47 +0530

    Added xpaths for product details page

--- src/test/java/pageObjects/b_ProductsDetailsPage.java
+++ src/test/java/pageObjects/b_ProductsDetailsPage.java
@@ -1,16 +1,40 @@
 package pageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class b_ProductsDetailsPage extends BasePage{
+public class b_ProductsDetailsPage extends BasePage {
 
 	public b_ProductsDetailsPage(WebDriver driver) {
 		super(driver);
 		PageFactory.initElements(driver, this);
-		
+
 	}
-	
-	
+
+	@FindBy(xpath = "//p[contains(text(), 'Premium Polo T-Shirts')]")
+	WebElement poloTshirt;
+
+	// xpath = "//div[@class='productinfo text-center'][.//p[text()='Premium Polo
+	// T-Shirts']]/ancestor::div[@class='product-image-wrapper']//div[@class='choose']//a[text()='View
+	// Product']"
+	@FindBy(xpath = "//div[@class='product-image-wrapper'][.//p[text()='Premium Polo T-Shirts']]//a[normalize-space()='View Product']")
+	WebElement viewProductOption;
+
+	@FindBy(xpath = "//div[@class='product-information']//*[contains(text(), 'Category')]")
+	WebElement productCategory;
+
+	@FindBy(xpath = "//div[@class='product-information']//*[contains(text(), 'Rs. ')]")
+	WebElement productPrice;
+
+	@FindBy(xpath = "//div[@class='product-information']//*[contains(text(), 'Availability:')]")
+	WebElement productAvailability;
+
+	@FindBy(xpath = "//div[@class='product-information']//*[contains(text(), 'Condition:')]")
+	WebElement productCondition;
+
+	@FindBy(xpath = "//div[@class='product-information']//*[contains(text(), 'Brand:')]")
+	WebElement productBrand;
 
 }
---
+++ src/test/java/stepDefinitions/b_AllProductsPage.java
@@ -0,0 +1,57 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class b_AllProductsPage {
+
+	@Given("user navigates to home page")
+	public void user_navigates_to_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("home page is visible")
+	public void home_page_is_visible() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user clicks on Products button")
+	public void user_clicks_on_products_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user should be navigated to All Products Page")
+	public void user_should_be_navigated_to_all_products_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("products list should be visible")
+	public void products_list_should_be_visible() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user clicks on View Products of {string} product")
+	public void user_clicks_on_view_products_of_product(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("product detail page should be displayed")
+	public void product_detail_page_should_be_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("product details should be visible")
+	public void product_details_should_be_visible() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
